b9ed027817d188894ebd50438f00f5ad8ce51e93
Cody Bisram <codyysb@gmail.com>
2021-02-18 19:33:24 -0500

    javascript responses to invalid search results

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -8,12 +8,16 @@ class StocksController < ApplicationController
           format.js { render partial: 'users/result' }
         end 
       else
-        flash[:alert] = "Please enter a valid ticker to search!"
-        redirect_to my_portfolio_path
+        respond_to do |format| 
+          flash.now[:alert] = "Please enter a valid ticker to search!"
+          format.js { render partial: 'users/result' }
+        end
       end
     else
-      flash[:alert] = "Please enter a ticker to search!"
-      redirect_to my_portfolio_path
+      respond_to do |format| 
+        flash.now[:alert] = "Please enter a ticker to search!"
+        format.js { render partial: 'users/result' }
+      end
     end
   end
 
